b600c348786d0dd46307b17899b7e0b3f96b1769
Sirajum Munir Prince <sirajummunirprince@gmail.com>
2021-04-20 22:15:14 +0600

    added port

--- application.py
+++ application.py
@@ -1,7 +1,12 @@
 from types import MethodType
 from flask import Flask, render_template
+import os
 app = Flask(__name__)
 
 @app.route("/")
 def index():
 	return render_template("index.html")
+
+if __name__ == "__main__":
+	port - int(os.environ.get("PORT", 5000))
+	app.run(debug=True, port=port)
